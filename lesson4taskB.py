7ed66877f93627ab759ee19bffa5edda095183d5
rekhert <rekhert@gmail.com>
2018-08-24 14:26:21 +0600

    lesson 4 task B

--- lesson4taskB.py
+++ lesson4taskB.py
@@ -10,9 +10,9 @@ word = input('Введите слово')
 def countword(text, word):
 
     if len(text) == 0:
-        raise Exception ('Текс не может быть пуст')
-    elif len(word) == 0 :
-        raise Exception ('Слово не может быть пустым')
+        raise Exception ('Текст не может быть пуст')
+    elif len(word) == 0:
+        raise Exception('Слово не может быть пустым')
     print (text.count(word))
 
 countword(text, word)
