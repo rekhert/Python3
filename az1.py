a3706f9ddaf94ff6806ed6852dbf85d8d6fc56b5
rekhert <rekhert@gmail.com>
2018-08-25 19:32:57 +0600

    AZ task 1

--- az1.py
+++ az1.py
@@ -6,6 +6,7 @@
 После ноля вывести количество всех чисел и сколько среди них четных
 
 добавить максимум и минимум
+второй максимум
 
 '''
 
@@ -13,8 +14,9 @@ a = int(input('введите число'))
 list = []
 count = 0
 even_count = 0
-mina = +10000
-maxa = -10000
+mina = a
+maxa = a
+maxa2 = a
 while a != 0:
     count += 1
     list.append(a)
@@ -23,10 +25,13 @@ while a != 0:
     if a < mina:
         mina = a
     if a > maxa:
+        max2 = maxa
         maxa = a
+    elif a > maxa2:
+        maxa2 = a
     a = int(input('введите число'))
 else:
     print("введен ноль, цикл остановлен", "\n кол-во введенных чисел до нуля:", count, "\n кол-во четных:", even_count,
-    "\n минимум", min(list), mina, "\n максимум", max(list), maxa)
+    "\n минимум", min(list), mina, "\n максимум", max(list), maxa, "\n второй макс:", max2)
 
 
