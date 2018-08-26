0d93b4c806f92ac6e353e98108c6709e5a7bf9f2
rekhert <rekhert@gmail.com>
2018-08-26 09:49:46 +0600

    lesson 5 task A

---
+++ lesson5taskA.py
@@ -0,0 +1,26 @@
+'''
+
+задание А
+
+Реализуйте функцию с возможностью отрпавить любое количество строк в качестве аргументов
+которая будет их конкатинировать, убирать лишние пробелы, корректировать регистр по умолчанию.
+Или в качестве альтернативы к каждой строке применять callback функцию со своим вариантом обработки строки.
+
+func("this is", "a    funCtion\'s argUMents", "     simple Example" callback = lambda x: x )
+"This is a funCtion's arguments simple example".
+
+'''
+
+import re
+
+
+def func(*ar):
+    s = ""
+    for i in ar:
+        s += " " + i
+
+    s = s.strip().capitalize()
+    print(re.sub("\s\s+", " ", s))
+
+
+func(" this is", "a    funCtion\'s argUMents", "     simple Example")
